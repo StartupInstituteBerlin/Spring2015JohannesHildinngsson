7b2e617d142e8552881ce053db03fc7665fc1bf0
Johannes <cyb@wigglytuff>
2015-03-06 18:20:47 +0100

    added models

---
+++ restaurant-rails/app/controllers/restaurants_controller.rb
@@ -0,0 +1,9 @@
+class RestaurantsController < ApplicationController
+
+def index
+ #@restaurant = Restaurant.getAll
+  #restaurant = restaurant.new
+  
+end
+
+end
---
+++ restaurant-rails/app/models/restaurant.rb
@@ -0,0 +1,9 @@
+class Restaurant < ActiveRecord::Base
+
+ def getAll
+   restu = restaurant.all
+   
+   #restaurant = restaurant.all
+ end
+ 
+end
