cdd283467f3258501eacc9fdec3c89e658817805
Johannes <cyb@wigglytuff>
2015-03-08 19:24:50 +0100

    added views and models

--- restaurant-rails/app/controllers/restaurants_controller.rb
+++ restaurant-rails/app/controllers/restaurants_controller.rb
@@ -1,9 +1,34 @@
 class RestaurantsController < ApplicationController
 
 def index
- #@restaurant = Restaurant.getAll
-  #restaurant = restaurant.new
+  @restaurant = Restaurant.all
   
+  
+end
+
+
+def show
+
+ @restaurant = Restaurant.find(params[:id])
+ 
+
+end
+
+
+def new
+end
+
+def create
+end
+
+def edit
+end
+
+def update
+end
+
+
+def destroy
 end
 
 end
--- restaurant-rails/app/models/restaurant.rb
+++ restaurant-rails/app/models/restaurant.rb
@@ -1,9 +1,5 @@
 class Restaurant < ActiveRecord::Base
 
- def getAll
-   restu = restaurant.all
-   
-   #restaurant = restaurant.all
- end
+
  
 end
