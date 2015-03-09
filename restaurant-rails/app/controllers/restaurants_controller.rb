e64c3708e0adc0a7a81012ed2d75a80e0966cfc1
Johannes <cyb@wigglytuff>
2015-03-09 12:39:06 +0100

    Updated controller with working actions

--- restaurant-rails/app/controllers/restaurants_controller.rb
+++ restaurant-rails/app/controllers/restaurants_controller.rb
@@ -2,33 +2,51 @@ class RestaurantsController < ApplicationController
 
 def index
   @restaurant = Restaurant.all
-  
-  
 end
-
 
 def show
 
  @restaurant = Restaurant.find(params[:id])
- 
-
 end
 
 
 def new
+  @restaurant = Restaurant.new
 end
 
 def create
+  @restaurant = Restaurant.new(restaurant_params)
+
+  @restaurant.save
+  redirect_to root_path
 end
 
 def edit
+  @restaurant = Restaurant.find(params[:id])
+ 
 end
 
 def update
+ @restaurant = Restaurant.find(params[:id])
+ 
+  if @restaurant.update(restaurant_params)
+    redirect_to @restaurant
+  else
+    render 'edit'
+  end
+
 end
 
 
 def destroy
+  @restaurant = Restaurant.destroy(params[:id])
+  redirect_to root_path
 end
+
+private
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :description, :address, :phonenumber)
+  end
+
 
 end
